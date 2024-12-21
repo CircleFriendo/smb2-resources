58aa1abdb42add79b0834a3b41bb7f01d9275ef3
CircleFriendo <131226495+CircleFriendo@users.noreply.github.com>
2024-12-21 14:26:45 -0600

    Update apply-patch.bat to change directory

--- apply-patch.bat
+++ apply-patch.bat
@@ -3,5 +3,8 @@ echo Apply xkas patch
 echo xkas-plus: https://github.com/devinacker/xkas-plus
 set /p patch="Enter patch name: "
 set /p rom="Enter ROM name: "
-xkas -o %rom% %patch%
+set dirxkas=%~dp0
+for %%a in (%patch%) do ( set dirpatch=%%~dpa )
+cd %dirpatch%
+"%dirxkas%xkas.exe" -o %rom% %patch%
 pause
