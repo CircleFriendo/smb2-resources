aeda8b0cac4c4283740973b7e4f995b0161acb6c
CircleFriendo <131226495+CircleFriendo@users.noreply.github.com>
2025-08-26 14:01:37 -0500

    Add files via upload

---
+++ patches/DisableStopwatchOnFiveVeggies.asm
@@ -0,0 +1,12 @@
+// xkas-plus
+// https://github.com/devinacker/xkas-plus
+
+// author: CircleFriendo
+
+// The fifth large veggie will no longer become a stopwatch
+
+arch nes.cpu
+
+
+org $0010EC
+    NOP
